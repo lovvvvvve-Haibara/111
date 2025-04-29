050576d14209a73189166ae2841384a306830087
excepy <liangc2004@outlook.com>
2025-04-30 00:03:32 +0800

    modified:   .gitignore 	new file:   dist/assets/CommunityView-BaNghTMw.js 	new file:   dist/assets/CommunityView-CQfFTaZQ.css 	new file:   dist/assets/DataView-DvX3s2Y-.css 	new file:   dist/assets/DataView-L8UkHOWd.js 	new file:   dist/assets/LayoutContainer-2-rtaSSg.css 	new file:   dist/assets/LayoutContainer-BOdXNI-d.js 	new file:   dist/assets/homeView-JkIiiOMY.js 	new file:   dist/assets/homeView-fieGhHlu.css 	new file:   dist/assets/index-4JXYnPmT.css 	new file:   dist/assets/index-BqCzZp7W.js 	new file:   dist/assets/index-DsGRImuB.css 	new file:   dist/assets/index-qPLI_qMi.js 	new file:   dist/assets/loginView-CgUk0TOy.css 	new file:   dist/assets/loginView-DiXUqD1P.js 	new file:   dist/assets/registerView-BBYzt8Ze.css 	new file:   dist/assets/registerView-Q0EtywgE.js 	new file:   dist/favicon.ico 	new file:   dist/index.html 	modified:   src/router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -32,12 +32,12 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: () => import('@/views/LoginView.vue')
+      component: () => import('@/views/loginView.vue')
     },
     {
       path: '/register',
       name: 'register',
-      component: () => import('@/views/RegisterView.vue')
+      component: () => import('@/views/registerView.vue')
     }
   ],
 })
